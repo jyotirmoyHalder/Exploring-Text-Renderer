e1103cc82589b236f3b2b355570b81e9825944d6
Jyotirmoy Halder <jyotirmoy.cse5.bu@gmail.com>
2026-08-01 17:19:36 +0600

    Initial Commit

---
+++ Exploring Text Renderer/Exploring_Text_RendererApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exploring_Text_RendererApp.swift
+//  Exploring Text Renderer
+//
+//  Created by jyotirmoy_halder on 1/8/26.
+//
+
+import SwiftUI
+
+@main
+struct Exploring_Text_RendererApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
