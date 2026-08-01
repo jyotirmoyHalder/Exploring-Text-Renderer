a9c591888bf49a309bd3560e355381f0ebfcf2f2
Jyotirmoy Halder <jyotirmoy.cse5.bu@gmail.com>
2026-08-01 17:39:06 +0600

    TextAttribute & TextRenderer

--- Exploring Text Renderer/ContentView.swift
+++ Exploring Text Renderer/ContentView.swift
@@ -8,15 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var blur = 1.0
+    let heart = Image(systemName: "heart.fill")
+    
+    var helloWorld: Text {
+        Text("Hello \(heart) World")
+            .font(.title3)
+            .fontWeight(.semibold)
+            .foregroundStyle(.red)
+            .customAttribute(CustomAttr())
+    }
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Yet another \"\(helloWorld)\" example.")
+                .font(.title)
+                .multilineTextAlignment(.center)
+                .textRenderer(Renderer(blur: blur))
+            
+            Spacer()
+            
+            Slider(value: $blur, in: 0...10)
         }
         .padding()
     }
+}
+
+struct Renderer: TextRenderer {
+    let blur: CGFloat
+    
+    func draw(layout: Text.Layout, in ctx: inout GraphicsContext) {
+        let allLines = layout
+            .flatMap({ $0 })
+        
+        for line in allLines {
+            // Extract custom attribute text
+            if line[CustomAttr.self] != nil {
+                var localContext = ctx
+                
+                let blurFilter = GraphicsContext.Filter.blur(radius: blur)
+                
+                localContext
+                    .addFilter(blurFilter)
+                
+                localContext.draw(line)
+            } else {
+                let localContext = ctx
+                localContext.draw(line)
+            }
+        }
+    }
+}
+
+struct CustomAttr: TextAttribute {
+    // Additional Properties
+    
 }
 
 #Preview {
